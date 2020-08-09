19a039e7997f35ae133cb23500285c56c00bc4b4
XiaofangChu <xfang.chu@gmail.com>
2020-08-08 23:03:54 -0700

    20200808 update notes

--- 0015-3Sum.py
+++ 0015-3Sum.py
@@ -50,6 +50,41 @@ class Solution:
         return list(map(list, res))  # trasform to list
 
 
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        nums.sort()
+        for i, v in enumerate(nums[:-2]):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            d = set()
+            # find target -v using 2 sum
+            for x in nums[i+1:]:
+                left = -v-x
+                if left not in d:
+                    d.add(x)
+                else:
+                    res.add((v, x, -v-x))
+        return res
+                    
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        nums.sort()
+        for i, v in enumerate(nums[:-2]):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i]+nums[l]+nums[r]
+                if s > 0:
+                    r -= 1
+                elif s < 0:
+                    l += 1
+                else:
+                    res.add((nums[i],nums[l],nums[r]))
+                    l, r = l+1, r-1
+        return res
+                    
 
 if __name__ == '__main__':
     s = Solution()
