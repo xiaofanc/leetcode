aa3005b27af109e91fe04bc49922931944a2652a
XiaofangChu <xfang.chu@gmail.com>
2020-08-08 22:18:22 -0700

    20200808 update notes

--- 0015-3Sum.py
+++ 0015-3Sum.py
@@ -1,6 +1,10 @@
 # Given an array nums of n integers, are there elements a, b, c in nums such
 # that a + b + c = 0? Find all unique triplets in the array which gives the sum
-# of zero. The solution set must not contain duplicate triplets.
+# of zero. 
+
+# The solution set must not contain duplicate triplets.
+
+# 2 pointers
 
 from typing import List
 #a+b+c = 0
