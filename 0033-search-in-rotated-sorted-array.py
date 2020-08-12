0177210ef8a52c2e699769aecd78a9c38db2b66b
XiaofangChu <xfang.chu@gmail.com>
2020-08-11 23:11:41 -0700

    20200811

---
+++ 0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,99 @@
+"""
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+"""
+
+class Solution: 
+    # one pass solution
+    # compare with middle and end to find out which part to search
+    def search(self, nums: List[int], target: int) -> int:
+        # modified binary search
+        start, end = 0, len(nums)-1
+        while start <= end:
+            mid = (start + end)//2
+            # stop point
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > nums[end]: #left part of mid is sorted
+                if target > nums[end] and target < nums[mid]:
+                    end = mid - 1
+                else: # target > nums[mid] or target <= nums[end]
+                    start = mid + 1
+            else: # right side of mid is sorted
+                if target <= nums[end] and target > nums[mid]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+        return -1
+
+
+    def search(self, nums: List[int], target: int) -> int:
+        # find the index of the smallest value using binary search.
+        def find_pivot_index(left, right):
+            if nums[left] < nums[right]:
+                return 0
+            
+            while left <= right:
+                pivot = (left + right)//2
+                #print(pivot)
+                if nums[pivot] > nums[pivot+1]:
+                    return pivot + 1 # pivot index
+                elif nums[pivot] > nums[left]: # search right part
+                    left = pivot
+                else: # search left part
+                    right = pivot   # could be a problem in second case if right = pivot - 1
+        
+        def search(left, right):
+            """
+            already find the left/right part of pivot to search
+            subarray is in ascending order
+            binary search for the target
+            """
+            while left <= right:
+                pivot = (left + right)//2
+                if nums[pivot] == target:
+                    return pivot
+                elif nums[pivot] > target:
+                    right = pivot - 1
+                else:
+                    left = pivot + 1
+            return -1
+        
+        n = len(nums)
+        if n == 0:
+            return -1
+        if n == 1:
+            return 0 if nums[0] == target else -1
+        
+        pivot_index = find_pivot_index(0, n-1)
+        #print(pivot_index)
+        if nums[pivot_index] == target:
+            return pivot_index
+        elif pivot_index == 0: # not rotated
+            return search(0, n-1)
+        elif target >= nums[0]: # search left part of pivot
+            return search(0, pivot_index)
+        else: # search right part of pivot
+            return search(pivot_index, n-1)              
+                    
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.search([4,5,6,7,0,1,2], 0))
+    print(s.search([4,5,1,2,3], 1))
+
+         
+                    
+        
+            
+            
+            
+        
+        
--- 0088-merge-sorted-array.py
+++ 0088-merge-sorted-array.py
@@ -23,5 +23,22 @@ class Solution:
             nums1[p1+p2:] = nums2[p2:]
         return nums1
 
+
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int):
+        i,j,k = m-1,n-1,m+n-1
+        while i >= 0 and j >= 0:
+            if nums1[i] < nums2[j]:
+                nums1[k] = nums2[j]
+                j -= 1
+            else:
+                nums1[k] = nums1[i]
+                i -= 1
+            k -= 1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+        return nums1
+
 s=Solution()
 print(s.merge([1,2,3,0,0,0],3,[2,5,6],3))
