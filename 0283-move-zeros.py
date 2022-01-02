a3a0bfff98e368415be465c7f3e01e6ffc9ad624
xiaofanc <xfang.chu@gmail.com>
2022-01-02 00:12:14 -0800

    update

--- 0283-move-zeros.py
+++ 0283-move-zeros.py
@@ -10,6 +10,24 @@ class Solution:
                 nums[i], nums[j] = nums[j], nums[i]
                 j += 1
         print(nums)    
-            
+
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                left = i
+                right = i + 1
+                break
+            if i == len(nums)-1:
+                return nums
+        while right <= len(nums)-1:
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
+        return nums
+                    
 s=Solution()
 print(s.moveZeroes([1,1,0,3,12]))
--- 0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements.py
@@ -1,3 +1,8 @@
+"""
+Time for heap:
+heappush/heappop: logn
+heapify: O(nlogn)
+"""
 class Solution:
     # Time: O(n+k)
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
@@ -11,6 +16,26 @@ class Solution:
         count = collections.Counter(nums)
         return heapq.nlargest(k, count.keys(), key=count.get)
 
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = collections.Counter(nums) # O(n)
+        heap = []
+        
+        # build a heap of size k using n elements - O(nlogk)
+        # height of the binary heap: logk
+        for key, value in count.items():
+            heapq.heappush(heap, (value, key)) 
+            if len(heap) > k: 
+                heapq.heappop(heap) # pop the small values
+        
+
+        # convert the heap into array - O(klogk)
+        res = [0] * k
+        for i in range(k-1, -1, -1):
+            res[i] = heapq.heappop(heap)[1] # get the key
+        return res
+
 if __name__ == '__main__':
     s = Solution()
     print(s.topKFrequent([1,1,1,2,2,3]), 2)
+
+
