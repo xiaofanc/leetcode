82a4c92b97b01910458967dfbd98aca57cecca31
xiaofanc <xfang.chu@gmail.com>
2022-03-29 23:53:12 -0700

    KMP

--- 0028-implement-strstr.py
+++ 0028-implement-strstr.py
@@ -1,20 +1,39 @@
+"""
+KMP:
+Example: 文本串：‘aabaab aafa’  模式串：‘aabaaf’
+长度为前1个字符的子串a，最长相同前后缀的长度为0。
+长度为前2个字符的子串aa，最长相同前后缀的长度为1。
+长度为前3个字符的子串aab，最长相同前后缀的长度为0。
+长度为前4个字符的子串aaba，最长相同前后缀的长度为1。
+长度为前5个字符的子串aabaa，最长相同前后缀的长度为2。
+长度为前6个字符的子串aabaaf，最长相同前后缀的长度为0。
+前缀表： 0，1，0，1，2，0
+找到的不匹配的位置， 那么此时我们要看它的前一个字符的前缀表的数值是多少。
+为什么要前一个字符的前缀表的数值呢，因为要找前面字符串的最长相同的前缀和后缀。
+所以要看前一位的 前缀表的数值。
+前一个字符的前缀表的数值是2， 所有把下标移动到模式串下标2的位置继续比配. 'aa'已经匹配上了。
+
+"""
 class Solution:
-    def strStr0(self, haystack: str, needle: str) -> int:
-        for i in range(len(haystack) - len(needle) + 1):
-            if haystack[i:i+len(needle)] == needle[:]:
-                return i
+    # def strStr0(self, haystack: str, needle: str) -> int:
+    #     for i in range(len(haystack) - len(needle) + 1):
+    #         if haystack[i:i+len(needle)] == needle[:]:
+    #             return i
             
-        return -1
+    #     return -1
         
-    def strStr1(self, haystack: str, needle: str) -> int:     
-        return haystack.find(needle)
+    # def strStr1(self, haystack: str, needle: str) -> int:     
+    #     return haystack.find(needle)
     
-    def strStr2(self, haystack: str, needle: str) -> int:    
+    # Time limit exceed
+    def strStr2(self, haystack, needle): 
+        nexttable = self.getNext(needle)
+        print('nexttable', nexttable)
         i, j = 0, 0
         while i < len(haystack) and j < len(needle):
-            if haystack[i] != needle[j]:
-                i = i - j + 1
-                j = 0
+            while haystack[i] != needle[j]:
+                # needle pointer returns back to the end of prefix 'b'
+                j = nexttable[j-1]
                 #print(i, j)
                 continue
             else:
@@ -23,9 +42,38 @@ class Solution:
                 #print(i, j)
         
         return i - j if j == len(needle) else -1
-                
-                
+
+    # passed -> O(M+N)
+    def strStr3(self, haystack, needle): 
+        if len(needle) == 0:
+            return 0
+        nexttable = self.getNext(needle)
+        print('nexttable', nexttable)
+        j = 0
+        for i in range(len(haystack)): # O(N)
+            while j > 0 and haystack[i] != needle[j]:
+                j = nexttable[j-1]
+            if haystack[i] == needle[j]:
+                j += 1
+            if j == len(needle):
+                return i - len(needle) + 1
+        return -1
+
+    def getNext(self, t):
+        # create prefix table -> O(M)
+        nexttable = [0] * len(t)
+        # j = end of prefix, i = end of postfix
+        j = 0 
+        for i in range(1, len(t)):
+            while t[i] != t[j] and j > 0:
+                # j return back
+                j = nexttable[j-1]
+            if t[i] == t[j]:
+                j += 1
+            nexttable[i] = j
+        return nexttable
+
 s=Solution()
-print(s.strStr0("hello","ll"))
-print(s.strStr1("hello","ll"))
-print(s.strStr2("hello","ll"))
+# print(s.strStr0("hello","ll"))
+# print(s.strStr1("hello","ll"))
+print(s.strStr2("aabaabaaf","aabaaf"))
--- 0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern.py
@@ -21,3 +21,41 @@ class Solution:
         ss = (s+s)[1:-1]
         return ss.find(s) != -1
         # return s in (2*s)[1:-1]
+
+"""
+KMP:
+如果len % (len - (prefix_table[len - 1])) == 0 ，则说明 (数组长度-最长相等前后缀的长度) 正好可以被数组的长度整除，说明有该字符串有重复的子字符串。
+数组长度减去最长相同前后缀的长度相当于是第一个周期的长度，也就是一个周期的长度，如果这个周期可以被整除，就说明整个数组就是这个周期的循环。
+https://programmercarl.com/0459.%E9%87%8D%E5%A4%8D%E7%9A%84%E5%AD%90%E5%AD%97%E7%AC%A6%E4%B8%B2.html#%E5%85%B6%E4%BB%96%E8%AF%AD%E8%A8%80%E7%89%88%E6%9C%AC
+"""
+
+    def repeatedSubstringPattern2(self, s: str) -> bool:
+        if not s:
+            return False
+        prefix_table = self.getNext(s)
+        # len(s)-prefix_table[-1] -> 第一个周期的长度
+        if prefix_table[-1] != 0 and len(s) % (len(s)-prefix_table[-1]) == 0:
+            return True
+        return False
+    
+    def getNext(self, s):
+        j = 0
+        # prefix_table[i]：当前位置最长相同前缀后缀长度
+        prefix_table = [0] * len(s)
+        for i in range(1, len(s)):
+            while s[j] != s[i] and j > 0:
+                j = prefix_table[j-1]
+            if s[j] == s[i]:
+                j += 1
+            prefix_table[i] = j
+        return prefix_table
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.repeatedSubstringPattern2("abab")) # True
+    # prefix_table: [0,0,0,0,1,2,3,4,5,6,7,8] # 12 % (12-8) == 0
+    print(s.repeatedSubstringPattern2("asdfasdfasdf")) # True
+
+
+
+
