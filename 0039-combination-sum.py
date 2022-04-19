74243d1b94dc3bd3a9bdbfc8e9ce87235d94434a
xiaofanc <xfang.chu@gmail.com>
2022-04-18 21:32:10 -0700

    combination sum

--- 0039-combination-sum.py
+++ 0039-combination-sum.py
@@ -58,6 +58,22 @@ class Solution:
         
         backtrack(0, target, [])
         return res
+ 
+     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        n = len(candidates)
+        
+        def dfs(start, remain, comb):
+            if remain < 0:
+                return
+            if remain == 0:
+                res.append(comb)
+                return
+            for i in range(start, n):
+                dfs(i, remain-candidates[i], comb + [candidates[i]])
+                
+        dfs(0, target, [])
+        return res       
         
 if __name__ == '__main__':
     s = Solution()
---
+++ 0078-subsets.py
@@ -0,0 +1,37 @@
+"""
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+"""
+class Solution:
+    # DFS
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        
+        def dfs(start, comb):
+            res.append(comb)
+            for i in range(start, n):
+                dfs(i+1, comb+[nums[i]])
+        dfs(0, [])
+        return res
+
+    # backtrack
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        
+        def backtrack(start, comb):
+            res.append(comb[:])
+            for i in range(start, n):
+                comb.append(nums[i])
+                backtrack(i+1, comb)
+                comb.pop()
+        backtrack(0, [])
+        return res
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.subsets([1,2,3]))
