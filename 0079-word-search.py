fc0018c3729124008a28f84929958ae022547c00
xiaofanc <xfang.chu@gmail.com>
2022-04-20 22:58:24 -0700

    update

--- 0079-word-search.py
+++ 0079-word-search.py
@@ -1,3 +1,11 @@
+"""
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring.
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+
+"""
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         directions = [(0,1),(0,-1),(1,0),(-1,0)]
