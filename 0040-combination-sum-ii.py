a9024b85a6b47648d3a2e88a4e5f71820e8f77f5
xiaofanc <xfang.chu@gmail.com>
2022-04-22 22:22:07 -0700

    backtracking

--- 0040-combination-sum-ii.py
+++ 0040-combination-sum-ii.py
@@ -87,6 +87,29 @@ class Solution:
         dfs(0, target, [])
         return res  
 
+    # do not use i > start
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        n = len(candidates)
+        candidates.sort()  
+        
+        def backtrack(index, comb, remain):
+            if remain == 0:
+                res.append(comb[:])
+            if index >= n or remain < 0:
+                return            
+            prev = -1
+            for i in range(index, n):
+                if candidates[i] == prev:
+                    continue
+                comb.append(candidates[i])
+                backtrack(i+1, comb, remain-candidates[i])
+                comb.pop()
+                prev = candidates[i]
+
+        backtrack(0, [], target)
+        return res 
+
 if __name__ == '__main__':
     s = Solution()
     print(s.combinationSum2([10,1,2,7,6,6,1,5], 8))
