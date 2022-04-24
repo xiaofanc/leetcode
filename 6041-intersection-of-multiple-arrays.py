b9948fc0e4cfbf5bd4bd370b5367d3d309c321f5
xiaofanc <xfang.chu@gmail.com>
2022-04-23 21:12:25 -0700

    contest

--- 6041-intersection-of-multiple-arrays.py
+++ 6041-intersection-of-multiple-arrays.py
@@ -1,5 +1,7 @@
 """
 Given a 2D integer array nums where nums[i] is a non-empty array of distinct positive integers, return the list of integers that are present in each array of nums sorted in ascending order.
+Input: nums = [[3,1,2,4,5],[1,2,3,4],[3,4,5,6]]
+Output: [3,4]
 """
 
 class Solution:
@@ -10,3 +12,7 @@ class Solution:
             common = common.intersection(num)
         common = sorted(list(common))
         return common
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.intersection([[3,1,2,4,5],[1,2,3,4],[3,4,5,6]])) # [3,4]
