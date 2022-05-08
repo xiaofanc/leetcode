9ffcebcc0c2eec425be6f494b28b1510d8ac6034
xiaofanc <xfang.chu@gmail.com>
2022-05-08 13:23:38 -0700

    practice

--- 0072-edit-distance.py
+++ 0072-edit-distance.py
@@ -1,3 +1,31 @@
+"""
+base cases: 
+1. word1 = "", word2 = "abc", res = len(word2)
+2. word1 = "abc", word2 = "", res = len(word1)
+3. word1 = "", word2 = "", res = 0
+        i
+word1: "abc"
+word2: "adc"
+        j
+if word1[i] == word2[j]:
+    (i+1, j+1)
+else: 1 + min(...)
+insert: (i, j+1)
+delete: (i+1, j)
+replace: (i+1, j+1)
+
+dp[i][j]: min number of operations that convert word1[i:] to word2[j:]
+
+example:
+    word1 = "horse", word2 = "ros"
+       h  o  r  s  e 
+r    [[3, 3, 2, 3, 3, 3],  # dp[0][5] = convert "" to "ros" = 3
+o     [3, 2, 2, 2, 2, 2],  # dp[0][4] = convert "" to "os" = 2
+s     [4, 3, 2, 1, 1, 1], 
+      [5, 4, 3, 2, 1, 0]]
+          # dp[3][1] = convert "orse" to "" = 4
+
+"""
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         L1, L2 = len(word1)+1, len(word2)+1
@@ -13,3 +41,6 @@ class Solution:
 if __name__ == '__main__':
     s = Solution()
     print(s.minDistance("horse","ros") == 3)
+
+
+    
