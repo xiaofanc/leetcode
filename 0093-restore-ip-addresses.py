428a9f225af6e43c5c94c59d9a3960bc2c5d9871
xiaofanc <xfang.chu@gmail.com>
2022-05-08 19:59:16 -0700

    backtracking

--- 0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses.py
@@ -1,5 +1,19 @@
+"""
+There are 3 available spots to place the first dot
+           25525511135
+first dot:    2           55         255
+second dot: 5 55 552   5 52 525    2 25 255
+....
+There are 4 dots at most. We will return if there are more than 4 integers
+
+Time: 3^4
+Space: constant
+
+"""
+
+
 class Solution:
-    # Time complexity : as discussed above, there is not more than 27 combinations to check.
+    # Time complexity : as discussed above, there is not more than 3x3x3 = 27 combinations to check.
     # Space complexity : constant space to keep the solutions, not more than 19 valid IP addresses.
     def restoreIpAddresses(self, s: str) -> List[str]:
         def valid(segment):
@@ -26,7 +40,29 @@ class Solution:
         segments, output = [], []
         backtrack()
         return output
-                        
+
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        res = []
+        # length of s cannot be > 12
+        if len(s) > 12:
+            return res
+        
+        def backtrack(i, dots, curIP):
+            # i is the start position of substring
+            if dots == 4 and i == len(s):
+                res.append(curIP[:-1])
+                return
+            if dots > 4:
+                return
+            # j is the end position of the substring - dot position
+            # iterate over three available slots to place a dot if len(s[i:j+1]) > 3 then it will be larger than 255
+            for j in range(i, min(i+3, len(s))):
+                # start position can be 0 only if the segment is "0"
+                if int(s[i:j+1]) < 256 and (s[i] != "0" or i == j):
+                    backtrack(j+1, dots+1, curIP + s[i:j+1] + ".")
+        backtrack(0, 0, "")
+        return res                        
+
 if __name__ == '__main__':
     s = Solution()
     print(s.restoreIpAddresses("25525511135")) # ["255.255.11.135","255.255.111.35"]
