145f8bd1787c77eadd681be90496c61601acc977
xiaofanc <xfang.chu@gmail.com>
2022-05-19 19:36:05 -0700

    tree

--- 0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves.py
@@ -1,3 +1,8 @@
+"""
+首先要注意是判断左叶子，不是二叉树左侧节点.
+因为题目中其实没有说清楚左叶子究竟是什么节点，那么我来给出左叶子的明确定义：如果左节点不为空，且左节点没有左右孩子，那么这个节点的左节点就是左叶子.
+
+"""
 class TreeNode:
 	def __init__(self, val, left=None, right=None):
 		self.val = val
@@ -19,6 +24,12 @@ class Solution:
 		
 		
 	def sumOfLeftLeaves(self, root: TreeNode) -> int:
+		"""
+		1. we use a list to store nodes and res to store sum of left leaves
+		2. for every node, check if it has left node and right node
+		3. if it has left node, append it to the list and if it is a leaf, add to the res
+		4. if it has right node, append it (a right node can have left leaf)
+		"""
 		if not root:
 			return 0
 		res = 0
