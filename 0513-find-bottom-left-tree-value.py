083475bcf1918cb5feba08c2218f85971d9c9beb
xiaofanc <xfang.chu@gmail.com>
2022-05-20 14:23:34 -0700

    tree

--- 0513-find-bottom-left-tree-value.py
+++ 0513-find-bottom-left-tree-value.py
@@ -31,3 +31,30 @@ class Solution:
             res = [node.val for node in level]
             level = [c for node in level for c in [node.left, node.right] if c]
         return res[0]
+
+    # recursion
+    # 当遇到叶子节点的时候，就需要统计一下最大的深度了，所以需要遇到叶子节点来更新最大深度
+    # 那么如果找最左边的呢？可以使用前序遍历，这样才先优先左边搜索，然后记录深度最大的叶子节点，此时就是树的最后一行最左边的值
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        maxdepth, leftmostvalue = -float("inf"), 0
+        def traversal(node, depth):
+            # 全局变量
+            nonlocal maxdepth, leftmostvalue
+            if not node.left and not node.right:
+                if depth > maxdepth:
+                    maxdepth = depth
+                    leftmostvalue = node.val
+            if node.left:
+                depth += 1
+                traversal(node.left, depth)  # traversal(node.left, depth+1)
+                depth -= 1
+            if node.right:
+                depth += 1
+                traversal(node.right, depth)
+                depth -= 1
+        traversal(root, 0)
+        return leftmostvalue
+
+
+
+
