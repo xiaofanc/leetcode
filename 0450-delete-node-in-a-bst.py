6c77b65cc7eebc15140a12df30443aef9986aa7a
xiaofanc <xfang.chu@gmail.com>
2022-05-23 13:43:53 -0700

    BST

--- 0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst.py
@@ -8,5 +8,87 @@
 第三种情况：删除节点的左孩子为空，右孩子不为空，删除节点，右孩子补位，返回右孩子为根节点
 第四种情况：删除节点的右孩子为空，左孩子不为空，删除节点，左孩子补位，返回左孩子为根节点
 第五种情况：左右孩子节点都不为空，则将删除节点的左子树头结点（左孩子）放到删除节点的右子树的最左面节点的左孩子上，返回删除节点右孩子为新的根节点。
+what if remove root?
+
+普通二叉树的删除方式（没有使用搜索树的特性，遍历整棵树），用交换值的操作来删除目标节点。
+代码中目标节点（要删除的节点）被操作了两次：
+第一次是和目标节点的右子树最左面节点交换。
+第二次直接被NULL覆盖了。
 """
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+        if root.val == key:
+            if not root.left and not root.right:
+                return None
+            elif root.left and not root.right:
+                return root.left
+            elif root.right and not root.left:
+                return root.right
+            else: 
+                cur = root.right
+                # 找右子树最左面的节点
+                # 将删除节点的左子树头结点（左孩子）放到删除节点的右子树的最左面节点的左孩子上
+                while cur.left:
+                    cur = cur.left
+                cur.left = root.left
+                return root.right
+        # 这里相当于把新的节点返回给上一层，上一层就要用 root->left 或者 root->right接住
+        if root.val > key: root.left = self.deleteNode(root.left, key)
+        if root.val < key: root.right = self.deleteNode(root.right, key)
+        return root
+
+
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+        if root.val == key:
+            if not root.left and not root.right:
+                return None
+            elif root.left and not root.right:
+                return root.left
+            elif root.right and not root.left:
+                return root.right
+            else:
+                cur = root.right
+                while cur.left:
+                    cur = cur.left
+                # 将删除节点与右子树最左孩子交换值，并删除右子树最左孩子
+                root.val = cur.val
+                root.right = self.deleteNode(root.right, root.val)
+                return root
+        if root.val > key: root.left = self.deleteNode(root.left, key)
+        if root.val < key: root.right = self.deleteNode(root.right, key)
+        return root
+
+    # 普通二叉树的删除方式
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+        if root.val == key:
+            if not root.left and not root.right:
+                return None
+            elif not root.right:
+                return root.left
+            else:
+                cur = root.right
+                while cur.left:
+                    cur = cur.left
+                # 将删除节点与右子树最左孩子交换值，并删除右子树最左孩子
+                root.val = cur.val
+                root.right = self.deleteNode(root.right, root.val)
+                return root
+        root.left = self.deleteNode(root.left, key)
+        root.right = self.deleteNode(root.right, key)
+        return root
+
+
 
--- 0669-trim-a-binary-search-tree.py
+++ 0669-trim-a-binary-search-tree.py
@@ -1,3 +1,7 @@
+"""
+
+
+"""
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -23,8 +27,50 @@ class Solution:
                 return node
             
         return trim(root)
-        
-            
+
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        # Base Case
+        if not root:
+            return None
+        # 单层递归逻辑
+        # 若当前root节点小于左界：只考虑其右子树，用于替代更新后的其本身，抛弃其左子树整体
+        elif root.val < low: 
+            return self.trimBST(root.right, low, high)
+        # 若当前root节点大于右界：只考虑其左子树，用于替代更新后的其本身，抛弃其右子树整体
+        elif root.val > high:
+            return self.trimBST(root.left, low, high)
+        else:
+            # root->left接入符合条件的左孩子
+            root.left = self.trimBST(root.left, low, high)
+            root.right = self.trimBST(root.right, low, high)
+            return root       
+
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        # 处理头结点，让root移动到[L, R] 范围内，注意是左闭右闭
+        while root and (root.val < low or root.val > high):
+            if root.val < low:
+                root = root.right
+            if root.val > high:
+                root = root.left
+        cur = root
+        # 此时root已经在[L, R] 范围内，处理左孩子元素小于L的情况
+        while cur:
+            while cur.left and cur.left.val < low:
+                cur.left = cur.left.right
+            cur = cur.left
+        cur = root
+        # 此时root已经在[L, R] 范围内，处理右孩子大于R的情况
+        while cur:
+            while cur.right and cur.right.val > high:
+                cur.right = cur.right.left
+            cur = cur.right
+        return root            
+
 if __name__ == '__main__':
     s = Solution()
     print(s.trimBST([1,0,2], 1, 2) == [1,null,2])
+
+
+
