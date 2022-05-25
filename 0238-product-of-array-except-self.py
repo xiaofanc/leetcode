7738efa72f8a02627b282ce3ba4abc8a100decac
xiaofanc <xfang.chu@gmail.com>
2022-05-25 13:03:55 -0700

    blind75

--- 0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self.py
@@ -5,6 +5,18 @@ from itertools import accumulate
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+        
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
         count_0 = nums.count(0)
         if count_0 > 1:
             return [0]*len(nums)
