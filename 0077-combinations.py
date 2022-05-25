5e959ed23849c6277b4dbf19aff6a6e2d997c1b3
xiaofanc <xfang.chu@gmail.com>
2022-05-24 23:32:46 -0700

    update

--- 0077-combinations.py
+++ 0077-combinations.py
@@ -20,7 +20,9 @@ class Solution:
         def backtrack(first = 1, curr = []):
             if len(curr) == k:
                 output.append(curr[:])
-            for i in range(first, n+1):
+            # 剪枝, last start index 至少从3开始
+            last = n - (k-len(comb)) + 1 
+            for i in range(first, last+1):
                 curr.append(i)
                 backtrack(i+1, curr)
                 curr.pop()
