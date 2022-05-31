e197aba7e0d3ad1530bab1561a1d8444e8221413
xiaofanc <xfang.chu@gmail.com>
2022-05-30 22:29:33 -0700

    blind75

--- 0338-counting-bits.py
+++ 0338-counting-bits.py
@@ -1,7 +1,20 @@
 class Solution:
     def countBits(self, num: int) -> List[int]:
-        return [bin(i).count("1") for i in range(num+1)]
-        
+        return [bin(i).count("1") for i in range(num+1)] # '0b'
+
+class Solution:
+    # O(N)
+    # explanation: https://www.youtube.com/watch?v=RyBM56RIWrM&list=PLot-Xpze53ldVwtstag2TL4HQhAnC8ATf&index=12
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n+1)
+        # the highest power of 2 so far
+        offset = 1
+        for i in range(1, n+1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i-offset]
+        return dp
+
 
 if __name__ == '__main__':
     s = Solution()
