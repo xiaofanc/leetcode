82b1b745504466b8905c376ca5e231ddf69e1169
xiaofanc <xfang.chu@gmail.com>
2022-06-01 16:04:06 -0700

    blind75

--- 0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence.py
@@ -1,6 +1,8 @@
 """
+sol1: backtrack - TLE
 Given an unsorted array of integers, find the length of longest increasing subsequence.
 
+sol2: 
 Input: [10,9,2,5,3,7,101,18]
 Output: 4 
 Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4. 
@@ -17,6 +19,7 @@ j = 4 nums[5](7) > nums[4](3) and 3   2+1: same length  {2,3,7}
 """
 
 class Solution: # time: O(n^2)
+    # L[i]: longest increasing subsequence that ends with nums[i]
     def lengthOfLIS(self, nums: List[int]) -> int:
         if not nums:
             return 0
@@ -26,6 +29,33 @@ class Solution: # time: O(n^2)
                 if nums[i] > nums[j] and L[i] < L[j]+1:
                     L[i] = L[j]+1
         return max(L)
+
+    # LIS[i] = longest increasing subsequence that starts with nums[i]
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        LIS = [1] * len(nums)
+        for i in range(len(nums)-2, -1, -1):
+            for j in range(i+1, len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1+LIS[j])
+        return max(LIS)
+
+    # TLE
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        res = 1
+        def backtrack(index, comb):
+            nonlocal res
+            res = max(res, len(comb))
+            # print("comb->", comb[:])
+            if index == len(nums):
+                return
+            for i in range(index, len(nums)):
+                if comb == [] or nums[i] > comb[-1]:
+                    comb.append(nums[i])
+                    # print("comb", comb)
+                    backtrack(i+1, comb)
+                    comb.pop()
+        backtrack(0, [])
+        return res
 
 if __name__ == '__main__':
     s = Solution()
