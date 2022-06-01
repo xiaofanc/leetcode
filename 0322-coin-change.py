aa7b1a3940a3ffd341f39b0b1dedbe8465915251
xiaofanc <xfang.chu@gmail.com>
2022-06-01 11:53:42 -0700

    blind75

--- 0322-coin-change.py
+++ 0322-coin-change.py
@@ -1,22 +1,36 @@
 """
-find solution for 0-amount
+sol1: DFS/backtracking: TLE
+
+sol2: top down dynamic programming 
+coins: [1,2,5]
+what is the minimum number of coins to sum up to 0?
 dp(0) = 0
 dp(1) = 1
 dp(2) = 1+dp(1)
-dp(3) = min(1+dp(2), 2+dp(1))
-dp(4) = min(1+dp(3), 2+dp(2))
+dp(3) = min(1+dp(3-1), 1+dp(3-2))
+dp(4) = min(1+dp(4-1), 1+dp(4-2))
 ...
-dp(7) = min(1+dp(6), 2+dp(5), 5+dp(2))
+dp(7) = min(1+dp(7-1), 1+dp(7-2), 1+dp(7-5))
 
-dp(i) = -1 表示无解
+dp(i)可以无解
 
 """
 
 class Solution:  # time O(n*k) k=# of coins  n=0-amount
     def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount+1] * (amount+1) # start from 0, dp[i] < amount+1
+        dp[0] = 0
+        
+        for i in range(1, amount+1):
+            for c in coins:
+                if i-c >= 0:
+                    dp[i] = min(dp[i], 1+dp[i-c])
+        return dp[amount] if dp[amount] != amount+1 else -1
+
+    def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [-1]*(amount+1)
         dp[0] = 0
-        #dp[i] is the min combination for amount i
+        #dp[i] is the min combination sums up to amount i
         for amt in range(1, amount+1):
             minc = float('inf')
             for c in coins:
@@ -33,7 +47,7 @@ class Solution:  # time O(n*k) k=# of coins  n=0-amount
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [-1]*(amount+1)
         dp[0] = 0
-        #dp[i] is the min combination for amount i
+        #dp[i] is the min combination sums up to amount i
         for amt in range(1, amount+1):
             options = [dp[amt-c] for c in coins if amt-c >= 0 and dp[amt-c] != -1]
             if options != []:
