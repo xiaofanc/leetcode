41be3ef5a734444c4d6e5ecb934b10f2041c3c6f
xiaofanc <xfang.chu@gmail.com>
2022-06-10 22:53:53 -0700

    blind75

--- 0003-longest-substring-without-repeating-character.py
+++ 0003-longest-substring-without-repeating-character.py
@@ -1,5 +1,11 @@
 """
 sliding window:
+Solution 1:
+Use hashset to store the char visited
+If the right char is in the hashset: move the left pointer until char is not in the hashset
+Update the res
+
+Solution 2:
 We use Hashmap to store the characters index in current window [i,j)
 Then we slide the index j to the right
     - if it is not in the Hashmap, we slide j further
@@ -12,12 +18,22 @@ Then we slide the index j to the right
 """
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        visited = set()
+        l = res = 0
+        for r in range(len(s)):
+            while s[r] in visited:
+                visited.remove(s[l])
+                l += 1
+            visited.add(s[r])
+            res = max(res, r-l+1)
+        return res
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
         if len(s) <= 1:
             return len(s)
         # hashmap to store the index of char
         hashMap = {}
-        l = r = 0
-        res = 0
+        res = l = 0
         for r in range(len(s)):
             char = s[r]
             if char not in hashMap:
@@ -28,41 +44,6 @@ class Solution:
             # print("l,r", l, r)
             res = max(res, r-l+1)
         return res
-        
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        ans, lo, hi = 0, 0, 0
-        charidx = {}
-        for hi in range(len(s)):
-            shi = s[hi]
-            if shi in charidx:
-                lo = max(lo, charidx[shi])
-            ans = max(ans, hi-lo+1)
-            charidx[shi] = hi+1
-        return ans
-
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        low = 0
-        ans = 0
-        chars = dict()
-        
-        for high in range(len(s)):
-            shi = s[high]
-            if shi in chars:
-                low = max(chars[shi]+1, low)
-            chars[shi] = high
-            ans = max(ans, high-low+1)
-        return ans
-        
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        ans, lo, hi = 0, 0, 0
-        charidx = {}
-        for hi in range(len(s)):
-            if s[hi] in charidx and lo <= charidx[s[hi]]: # shi show again
-                lo = charidx[s[hi]] + 1  # cut off the previous occurrence 
-            else:
-                ans = max(ans, hi-lo+1)
-            charidx[s[hi]] = hi
-        return ans
 
     def lengthOfLongestSubstring(self, s: str) -> int:
         chars = [0] * 128
