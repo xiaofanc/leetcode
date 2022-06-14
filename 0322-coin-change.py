0a677f8681937a7ea341fa99ab79508791d89e25
xiaofanc <xfang.chu@gmail.com>
2022-06-14 10:58:19 -0700

    dp

--- 0322-coin-change.py
+++ 0322-coin-change.py
@@ -28,6 +28,14 @@ class Solution:  # time O(n*k) k=# of coins  n=0-amount
         return dp[amount] if dp[amount] != amount+1 else -1
 
     def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount+1] * (amount+1)
+        dp[0] = 0
+        for coin in coins:
+            for amt in range(coin, amount+1):
+                dp[amt] = min(dp[amt], 1+dp[amt-coin])
+        return dp[amount] if dp[amount] != amount+1 else -1
+            
+    def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [-1]*(amount+1)
         dp[0] = 0
         #dp[i] is the min combination sums up to amount i
---
+++ 0518-coin-change-2.py
@@ -0,0 +1,19 @@
+"""
+To understand how the coin loop being outside resolves the double counting, you should pay attention to the fact that when the coin loop is outside, we will count how many combinations are possible with say 2c coin and then add that number to the ways in which the same amount can be created using 3c coin. This imposition of order removes the double counting
+
+Compare that with the situation where the coins are in the inner loop. Here we would calculate how many ways we can create an amount using all coins (i.e. 1 and 2 both) and then for the next amount of interest, we will count using all coins (i.e. 1 and 2 both). As you see, we are counting both 1c after 2c and 2c after 1c scenarios, causing the double counting.
+"""
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount+1)
+        dp[0] = 1
+        for coin in coins:
+            for x in range(coin, amount+1):
+                dp[x] += dp[x-coin]
+                # print("dp", coin, dp)                
+        return dp[amount]
+
+if __name__ == '__main__':
+	s = Solution()
+	print(s.change(5, [1,2,5])) # 4
