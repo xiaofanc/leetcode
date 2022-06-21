b5454f313f4a38361b33b3e2d2cea6a7c54ee9f7
xiaofanc <xfang.chu@gmail.com>
2022-06-21 11:00:34 -0700

    blind75

--- 0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst.py
@@ -36,7 +36,36 @@ class Solution:
         for i in range(k):
             ans = next(g)
         return ans
-        
+
+    # recursion
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # use inorder traversal to traverse the tree
+        def inorder(node):
+            nonlocal k
+            if not node: # if node condition fails if node is 0
+                return 0
+            left = inorder(node.left)
+            k -= 1
+            if not k:
+                return node.val
+            right = inorder(node.right)
+            return left or right
+        return inorder(root)
+
+    # return None !!!
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # use inorder traversal to traverse the tree
+        def inorder(node):
+            nonlocal k
+            if not node: # if node condition fails if node is 0
+                return 0
+            inorder(node.left)
+            k -= 1
+            if not k:
+                return node.val
+            inorder(node.right)
+        return inorder(root)
+
 if __name__ == '__main__':
     s = Solution()
     print(s.kthSmallest([3,1,4,null,2], 1) == 1)
