fe0c991f80e6febebc199be74dc25be37cb82e68
xiaofanc <xfang.chu@gmail.com>
2022-06-30 22:04:39 -0700

    amazon

--- 0937-reorder-data-in-log-files.py
+++ 0937-reorder-data-in-log-files.py
@@ -9,6 +9,19 @@ class Solution:
             
         return sorted(logs, key=f)
 
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letterLogs, digitLogs = [], []
+        # split logs into two parts 
+        for log in logs:
+            if log.split()[1].isalpha():
+                letterLogs.append(log)
+            else:
+                digitLogs.append(log)
+        # sort the letterLogs by their content, then by id
+        letterLogs = sorted(letterLogs, key = lambda x: (x.split()[1:], x.split()[0]))
+        # add two parts together
+        return letterLogs + digitLogs
+        
 s=Solution()
 logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
 print(s.reorderLogFiles(logs))
