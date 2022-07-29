7e328a7a0cbffd10f6513012f6b9299fda8c0304
xiaofanc <xfang.chu@gmail.com>
2022-07-29 11:11:35 -0700

    update

--- 0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements.py
@@ -6,14 +6,14 @@ heappush/heappop: logn
 heapify: O(n)
 """
 class Solution:
-    # Time: O(n+k)
+    # Time: O(n+nlogn+k)
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         nums_dct = collections.Counter(nums)
         #print(sorted(nums_dct.items(), key=lambda item: -item[1]))
         res = [key for key, val in sorted(nums_dct.items(), key=lambda item: -item[1])]
         return res[:k]
 
-    # Time: O(nlogk)
+    # Time: O(n+nlogk)
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = collections.Counter(nums)
         return heapq.nlargest(k, count.keys(), key=count.get)
