61275498816bbcd291d6197252c65828e745a19c
xiaofanc <xfang.chu@gmail.com>
2022-07-29 15:22:24 -0700

    update

--- 0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings.py
@@ -28,6 +28,28 @@ class Codec:
             i = j+l+1
         return res
 
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        i = 0
+        res = []
+        while i < len(s):
+            pos = s.index("#", 1)  # position of first #
+            l = int(s[i:pos])
+            res.append(s[pos+1:pos+l+1])
+            s = s[pos+l+1:]
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        res = []
+        while s:
+            l, s = s.split("#", 1)  # 5, hello5#world
+            res.append(s[:int(l)])
+            s = s[int(l):]
+        return res
+        
 if __name__ == '__main__':
 	s = Codec()
 	enc = s.encode(["He7#llo","World"])
