e2b59d4be7a84201c0fb480d9d13b5525cbe5847
xiaofanc <xfang.chu@gmail.com>
2022-07-28 23:50:16 -0700

    update

--- 0091-decode-ways-2.py
+++ 0091-decode-ways-2.py
@@ -1,7 +1,12 @@
 """
 dp[i] = number of ways to decode s[i:]
+decode 0123: 0
+
 decode 12345:
 subproblems: 1+decode(2345), 12+decode(345)
+
+decode 2345: 2+decode(“345”), 23+decode(“45”)
+decode 2789: 2+decode(“789”)
 
 decode 3678:
 subproblems: 3+decode(678)
@@ -10,6 +15,23 @@ base case: dp[len(s)] = 1
 """
 
 class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = {len(s):1}
+        for i in range(len(s)-1,-1,-1):
+            if s[i] == "0":
+                dp[i] = 0
+            elif s[i] == "1":
+                dp[i] = dp[i+1]
+                if i < len(s)-1:
+                    dp[i] += dp[i+2]
+            elif s[i] == "2":
+                dp[i] = dp[i+1]
+                if i < len(s)-1 and s[i+1] <= '6':
+                    dp[i] += dp[i+2]
+            else:
+                dp[i] = dp[i+1]
+        return dp[0] 
+
     def numDecodings(self, s: str) -> int:
         dp = {len(s):1} # base case
         def dfs(i):
