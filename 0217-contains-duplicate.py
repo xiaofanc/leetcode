de0208de20985db6e92f8635f083de0db6e54583
xiaofanc <xfang.chu@gmail.com>
2022-07-28 23:57:08 -0700

    update

--- 0217-contains-duplicate.py
+++ 0217-contains-duplicate.py
@@ -7,6 +7,14 @@ class Solution(object):
         numsset = set(nums)
         return len(numsset) != len(nums)
 
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        visited = set()
+        for n in nums:
+            if n in visited:
+                return True
+            visited.add(n)
+        return False
+        
 if __name__ == '__main__':
 	s = Solution()
 	print(s.containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
