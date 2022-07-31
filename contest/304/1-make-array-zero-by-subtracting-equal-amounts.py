e0e6e65794f7daf2995fa8b6f1ee1d1ec0164b4d
xiaofanc <xfang.chu@gmail.com>
2022-07-31 13:44:27 -0700

    update

--- contest/304/1-make-array-zero-by-subtracting-equal-amounts.py
+++ contest/304/1-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,4 +1,11 @@
-
+"""
+Input: nums = [1,5,0,3,5]
+Output: 3
+Explanation:
+In the first operation, choose x = 1. Now, nums = [0,4,0,2,4].
+In the second operation, choose x = 2. Now, nums = [0,2,0,0,2].
+In the third operation, choose x = 2. Now, nums = [0,0,0,0,0].
+"""
 
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
@@ -13,6 +20,13 @@ class Solution:
                     nums[i] -= minn
             cnt += 1
         return cnt
-                
-            
-            
+
+    def minimumOperations(self, nums: List[int]) -> int:
+        # Same elements, are always same
+        # Different elements, are always different until 0
+        # count different numbers
+        return len(set(nums) - {0})
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.minimumOperations([1,5,0,3,5])) # 3
