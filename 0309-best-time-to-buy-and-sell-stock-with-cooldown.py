183e95ca9057a9714a0475c18ff55b0afc2a6a30
xiaofanc <xfang.chu@gmail.com>
2022-08-02 11:17:07 -0700

    update

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -26,8 +26,34 @@ class Solution:
             sell[i] = max( buy[i-1]+prices[i], sell[i-1])
             rest[i] = max(sell[i-1], buy[i-1], rest[i-1])
         return sell[-1]
-        
+
+    def maxProfit(self, nums: List[int]) -> int:
+        # (i, buying) to store if buy/sell in ith day
+        # if buy -> i+1
+        # if sell -> i+2
+        dp = {}
+        def dfs(i, buying):
+            if i >= len(nums):
+                return 0
+            if (i, buying) in dp:
+                return dp[(i, buying)]
+            if buying:
+                buy = dfs(i+1, not buying) - nums[i] # if buy, the next day cannot buy
+                cooldown = dfs(i+1, buying)  # if cooldown, the second day can still buy
+                dp[(i, buying)] = max(buy, cooldown)
+            else:
+                sell = dfs(i+2, not buying) + nums[i] # if sell, we must skip next day
+                cooldown = dfs(i+1, buying)
+                dp[(i, buying)] = max(sell, cooldown)
+            return dp[(i, buying)]
+        return dfs(0, True)
+
         
 if __name__ == '__main__':
     s = Solution()
     print(s.maxProfit([1,2,3,0,2]) == 3)
+
+
+
+
+    
