2c79ee2c7e93704924abe78100cda3482c32d915
xiaofanc <xfang.chu@gmail.com>
2022-08-02 23:12:58 -0700

    update

--- 0155-min-stack.py
+++ 0155-min-stack.py
@@ -1,9 +1,18 @@
 """
-
 min stack:
 store min value in the stack at the same time
 
+warning:
+do not use self.minval in the initialization:
+self.minval will change due to poping, therefore using the minval from the top
+of the stack !
+
+push, push, push, pop, push, getMin
+-10, 14, -20, [], -7, ?
+[(-10,-10),(14,-10),(-20,-20)]
+[(-10,-10),(14,-10),(-7,-10)]  (-7 should use -10 from previous, rather than -20 from self.minval)
 """
+
 class MinStack:
 
     def __init__(self):
