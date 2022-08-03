5c0957bc2e0f638298aa9dd838573d91cfe4af5d
xiaofanc <xfang.chu@gmail.com>
2022-08-03 12:22:46 -0700

    update

--- 0022-generate-parentheses.py
+++ 0022-generate-parentheses.py
@@ -5,13 +5,16 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         ans = []
         def backtrack(S = [], left=0, right=0):
+            # must have n left and right parenthesis
             if len(S) == 2*n:
                 ans.append("".join(S))
                 return
+            # left parenthesis cannot be more than n
             if left < n:
                 S.append("(")
                 backtrack(S, left+1, right)
                 S.pop()
+            # right parenthesis cannnot be more than left in the current combination
             if right < left:
                 S.append(")")
                 backtrack(S, left, right+1)
