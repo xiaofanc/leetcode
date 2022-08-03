a8766c39af480ee06953ceca22728e24f5a74e59
xiaofanc <xfang.chu@gmail.com>
2022-08-03 12:28:07 -0700

    update

--- 0022-generate-parentheses.py
+++ 0022-generate-parentheses.py
@@ -1,7 +1,12 @@
 """
 We can start an opening bracket if we still have one (of n) left to place. And we can start a closing bracket if it would not exceed the number of opening brackets.
+
+The height of the tree is 2n, since we must branch once per bracket. So the number of vertices is at most 2^2n and the number of leaves is at most half the number of vertices of a perfect tree, so asymptotically O(2^2n). Additionally we do linear work per leaf to copy the sequence to output.
+
+A more complete analysis would take into account that some bracket choices are invalid, which leads to asymptotically fewer leaves in the tree. However since we are interested in upper bounds, it's still correct to say that time complexity is O(n*(2^2n)), even though it's not the most accurate upper bound.
 """
 class Solution:
+    # Time: upper bound -> O(n*(2^2n))
     def generateParenthesis(self, n: int) -> List[str]:
         ans = []
         def backtrack(S = [], left=0, right=0):
@@ -26,3 +31,6 @@ class Solution:
 if __name__ == '__main__':
 	s = Solution()
 	print(s.generateParenthesis(3)) # ["((()))","(()())","(())()","()(())","()()()"]
+
+
+    
