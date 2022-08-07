9ce0e75ecf8cbd38366ac6a64acf784ab08cf5fd
xiaofanc <xfang.chu@gmail.com>
2022-08-07 13:56:39 -0700

    update

--- contest/305/3-check-if-there-is-a-valid-partition-for-the-arr.py
+++ contest/305/3-check-if-there-is-a-valid-partition-for-the-arr.py
@@ -20,22 +20,72 @@ class Solution:
                 return False
             if dp[i] != -1:
                 return dp[i]
+            res = False
             # condition-1
             if i+1 < len(nums) and nums[i] == nums[i+1]:
-                if solve(i+2): 
-                    return True
+                res = res or solve(i+2)
                 # condition-2
                 if i+2 < len(nums) and nums[i] == nums[i+2]:
-                    if solve(i+3): 
-                        return True
+                    res = res or solve(i+3)
             # condition-3
             if i+2 < len(nums) and nums[i+1] - nums[i] == 1 and nums[i+2] - nums[i+1] == 1:
-                if solve(i+3): 
-                    return True
-            dp[i] = False
-            return dp[i]
-        
+                res = res or solve(i+3)
+            dp[i] = res
+            return res
+
         return solve(0)
+
+    def validPartition(self, nums: List[int]) -> bool:
+        # dp[i] = if nums[i:] can be partitioned into valid subarrays
+        n = len(nums)
+        dp = [False] * (n+1)
+        dp[-1] = True
+        for i in range(n-1,-1,-1):
+            # condition-1
+            if i < n-1 and nums[i] == nums[i+1] and dp[i+2]:
+                dp[i] = True
+                continue
+            # condition-2
+            if i < n-2 and nums[i] == nums[i+1] == nums[i+2] and dp[i+3]:
+                dp[i] = True
+                continue
+            # condition-3
+            if i < n-2 and nums[i+1]-nums[i] == 1 and nums[i+2]-nums[i+1] == 1 and dp[i+3]:
+                dp[i] = True
+        return dp[0]
+
+    def validPartition(self, nums: List[int]) -> bool:
+        # dp[i] = if nums[:i+1] can be partitioned into valid subarrays
+        n = len(nums)
+        dp = [False] * (n)
+        if n <= 1:
+            return False
         
+        if nums[0] == nums[1]:
+            dp[1] = True
+        if n == 2: return dp[1]
+        
+        if nums[0] == nums[1] == nums[2]:
+            dp[2] = True
+        if nums[1]-nums[0] == 1 and nums[2]-nums[1] == 1:
+            dp[2] = True
+        if n == 3: return dp[2]
+        
+        for i in range(3, n):
+            if nums[i] == nums[i-1] and dp[i-2]:
+                dp[i] = True
+            if nums[i] == nums[i-1] == nums[i-2] and dp[i-3]:
+                dp[i] = True
+            if nums[i] - nums[i-1] == 1 and nums[i-1] - nums[i-2] == 1 and dp[i-3]:
+                dp[i] = True
+        return dp[-1]
+
+if __name__ == '__main__':
+     s = Solution()
+     print(s.validPartition([1,2])) 
+     print(s.validPartition([2,2]))                    # True
+     print(s.validPartition([473928,473929,473930]))   # True
+     print(s.validPartition([4,4,4,5,6]))              # True
+
             
                 
