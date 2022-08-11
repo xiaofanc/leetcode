da971a73dc44a581674e7d0dc1afc9ed238f2099
xiaofanc <xfang.chu@gmail.com>
2022-08-10 19:48:50 -0700

    update

--- 0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array.py
@@ -8,7 +8,7 @@ class Solution:
         l, r = 0, len(nums)-1
         res = float('inf')
         while l <= r:
-            # sorted
+            # if left to right is sorted or only 1 num left
             if nums[l] <= nums[r]:
                 res = min(res, nums[l])
             
@@ -32,7 +32,41 @@ class Solution:
                 r = m
         return nums[l]
 
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            if nums[l] <= nums[r]: # why =? [1],[2,1]
+                return nums[l]
+            m = l + (r-l) // 2
+            # if left is sorted, seach min in the right
+            # why no =? since only when l=r=m, m=r, it is handled before (if nums[l] <= nums[r])
+            if nums[m] > nums[r]: 
+                l = m + 1
+            else: # seach min in the left
+                r = m
+        return nums[l]
+
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums)-1
+        
+        while l <= r:
+            if nums[l] <= nums[r]:  # why =? : [1],[2,1]
+                return nums[l]
+            m = l + (r-l) // 2
+            # print("m", m, l, r)
+            # if left is sorted, seach min in the right
+            # why =? since l to r is not sorted, therefore when m == l, we need to search from m+1
+            if nums[m] >= nums[l]: 
+                l = m + 1
+            else: # seach min in the left, possible m is the min num
+                r = m
+        return nums[r]
+
+
 if __name__ == '__main__':
     s = Solution()
     print(s.findMin([2,1])) # 1 why must l <= r and nums[m] >= nums[l]
     print(s.findMin([3,4,5,1,2])) # 1
+
+
