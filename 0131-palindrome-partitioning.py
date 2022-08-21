3df23bed7ba1f9e18de0b760717913a8db5243fc
xiaofanc <xfang.chu@gmail.com>
2022-08-20 23:38:53 -0700

    update

--- 0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning.py
@@ -5,6 +5,8 @@ Output: [["a","a","b"],["aa","b"]]
 
 Time: O(N⋅2^N)
 This is the worst-case time complexity when all the possible substrings are palindrome.
+Detailed explanation of runtime complexity:
+https://leetcode.com/problems/palindrome-partitioning/solution/
 
 Space: O(N)
 store recursion stack
@@ -58,6 +60,25 @@ class Solution:
         backtrack(0, [], 0)
         return res
 
+    # backtracking with DP
+    def partition(self, s: str) -> List[List[str]]:
+        # dp: determine if a substring starting at index start and ending at index end is a palindrome or not
+        l = len(s)
+        dp = [[False for i in range(l)] for j in range(l)]
+        
+        res = []
+        def dfs(start, comb):
+            if start >= l:
+                res.append(comb[:])
+            for end in range(start, l):
+                if s[start] == s[end] and (end-start <= 2 or dp[start+1][end-1]):
+                    dp[start][end] = True  # memoization
+                    comb.append(s[start:end+1])
+                    dfs(end+1, comb)
+                    comb.pop()
+        dfs(0, [])
+        return res
+                    
 if __name__ == '__main__':
 	s = Solution()
 	print(s.partition("aabaa")) 
