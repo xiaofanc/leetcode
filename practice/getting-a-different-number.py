47e9cd827bea8d0d8a629ee7fb8119bcbc9667ae
xiaofanc <xfang.chu@gmail.com>
2022-08-26 18:41:26 -0700

    update

--- practice/getting-a-different-number.py
+++ practice/getting-a-different-number.py
@@ -1,5 +1,5 @@
 def get_different_number(arr):
-  # solution 1: sorting the arr and loop over the sorted arr (Time: O(nlogn+n0))
+  # solution 1: sorting the arr and loop over the sorted arr (Time: O(nlogn+n))
   # solution 2: put the numbers from the arr into a set, loop starting from 0
   #             if num is not set, return num (Time: O(n), Space: O(N))
   #             [10, 11, 12] --> 0 
@@ -14,3 +14,30 @@ def get_different_number(arr):
     if i not in num:
       return i
   return n
+
+# solution 3:
+# arr must contain [0...n-1]
+# cyclic replacement to move num into their corrent position
+def get_different_number(arr): # arr = [1,0,3,4,5]
+  if not arr:
+    return 0
+  n = len(arr)
+  
+  for i in range(n):
+    # update tmp to be the next num
+    tmp = arr[i]   
+    while tmp < n and tmp != arr[tmp]:
+      arr[tmp], tmp = tmp, arr[tmp]
+  #print("arr->", arr) [0,1,3,3,4]
+  for i in range(n):
+    if arr[i] != i:
+      return i
+  return n
+
+print(get_different_number([0])) # 1
+print(get_different_number([0,1,2])) # 3
+print(get_different_number([6,7,8])) # 0
+print(get_different_number([1,0,3,4,5])) # 2
+
+
+
