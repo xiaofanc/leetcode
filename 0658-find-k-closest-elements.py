6114ade2c1c96a597fa29d2222c87aa1a71c1e4a
xiaofanc <xfang.chu@gmail.com>
2022-09-29 10:36:27 -0700

    update

--- 0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements.py
@@ -41,12 +41,40 @@ class Solution:
             # move right pointer to +1
                 r += 1
             else:
-            # move left pointer to -1
+            # move left pointer to -1 when abs(arr[l]-x) <= abs(arr[r]-x)
                 l -= 1
         return arr[l+1: r]
 
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        # binary search find the closest element <= x
+        def binarySearch(l, r):
+            while l <= r:
+                m = l + (r-l)//2
+                if arr[m] == x:
+                    return m
+                elif arr[m] < x:
+                    l = m+1
+                else:
+                    r = m-1
+            return r
+        i = binarySearch(0, len(arr)-1)
+        j = i+1
+        while (i >= 0 or j <= len(arr)-1) and k > 0:
+            if i == -1:
+                j += 1
+            elif j == len(arr):
+                i -= 1
+            elif i >= 0 and j <= len(arr)-1:
+                if abs(arr[i]-x) <= abs(arr[j]-x):
+                    i -= 1
+                else:
+                    j += 1
+            k -= 1
+        return arr[i+1:j]
+
 if __name__ == '__main__':
 	s = Solution()
-	print(s.findClosestElements([1,2,3,4,5], 4, -1))
+	print(s.findClosestElements([1,2,3,4,5], 4, -1)) # [1,2,3,4]
+    print(s.findClosestElements([1,2,3,4,5], 4, 3))  # [1,2,3,4]
 
 
