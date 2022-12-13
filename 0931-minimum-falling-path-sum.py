67179006b4191f350cbf88e86080529361ccedc3
xiaofanc <xfang.chu@gmail.com>
2022-12-13 10:01:16 -0800

    update

---
+++ 0931-minimum-falling-path-sum.py
@@ -0,0 +1,14 @@
+
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        n = len(matrix)
+        for i in range(1, n):
+            for j in range(n):
+                if j == 0:
+                    matrix[i][j] += min(matrix[i-1][j], matrix[i-1][j+1])
+                elif j == n-1:
+                    matrix[i][j] += min(matrix[i-1][j-1], matrix[i-1][j])
+                else:
+                    matrix[i][j] += min(matrix[i-1][j-1], matrix[i-1][j], matrix[i-1][j+1])
+        return min(matrix[-1])
+
