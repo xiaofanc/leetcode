35908e5dc6e16b40e6d15ed081a999189a86935b
xiaofanc <xfang.chu@gmail.com>
2022-12-26 22:17:54 -0800

    update

--- 2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum.py
@@ -19,3 +19,39 @@ class Solution:
                     break
             res[dct[q]] = i
         return res
+
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        
+        nums.sort()
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+
+        res = []
+        for q in queries:
+            idx = bisect.bisect_right(nums, q)
+            res.append(idx)
+        return res
+
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        res = []
+        nums.sort()
+        for q in queries:
+            s = 0
+            for i, n in enumerate(nums):
+                if s+n <= q:
+                    s += n
+                else:
+                    res.append(i)
+                    break
+            else:
+                res.append(len(nums))
+        return res
+                    
+                    
+                    
+            
+            
+                        
+                
+
+        
